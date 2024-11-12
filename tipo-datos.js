eb48a88f273d5302302abdd44b328a6e76bc3122
Kevin Sanchez <kevinsan1835@gmail.com>
2024-11-12 16:45:51 -0500

    tipos de datos

---
+++ tipo-datos.js
@@ -0,0 +1,26 @@
+
+//let: variables cuyo valor puede cambiar
+let edad = 20;
+edad = 21;  // válido
+
+//const: valores que no deben cambiar
+const PI = 3.1416;
+// PI = 3.14; // Error
+
+//var:  Se puede reasignar como let
+var nombre = "Ana";
+nombre = "Luis"; // válido
+
+//Números (number)
+let edad2 = 24;
+const precio = 19.99;
+
+//cadenas de texto (string)
+let nombre = "Carlos";
+const saludo = `Hola, ${nombre}`;
+
+//Booleanos (boolean)
+let esEstudiante = true;
+const estaLogueado = false;
+
+
